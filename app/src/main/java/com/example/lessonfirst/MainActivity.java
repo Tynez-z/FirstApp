d98cccc07081f81c2a7ab92f84b26c2016c4a3ae
Tanya <tatyana.kazey1@gmail.com>
2021-04-11 17:21:42 +0300

    Array+Collections

--- app/src/main/java/com/example/lessonfirst/MainActivity.java
+++ app/src/main/java/com/example/lessonfirst/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.lessonfirst;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
